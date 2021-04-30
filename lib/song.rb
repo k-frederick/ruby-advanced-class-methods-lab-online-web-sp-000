27dbc6717fd93b4e88abe34b64c8066e222d3afd
Kristina Frederick <kristinafrederick@icloud.com>
2021-04-30 06:04:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     order = self.all.sort_by {|song| song.name}
     order
   end
-  def new_from_filename(filename)
+  def self.new_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
@@ -48,8 +48,12 @@ class Song
     song.artist_name = song_array[0]
     song
   end
-  def create_from_filename(filename)
-
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
   end
   def self.destroy_all
     @@all = []
