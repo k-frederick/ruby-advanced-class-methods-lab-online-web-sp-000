d03dd370bfa950fdd0314ea953951ac847a3521b
Kristina Frederick <kristinafrederick@icloud.com>
2021-04-30 05:46:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,25 @@ class Song
   def save
     self.class.all << self
   end
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+  def find_by_name
+
+  end
+  def find_or_create_by_name
+  end
 
 end
